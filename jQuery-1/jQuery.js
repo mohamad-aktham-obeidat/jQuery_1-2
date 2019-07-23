201a1dc53cb6fe6958d68fb6f27c3401addf0f1b
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-07-23 11:32:18 +0300

    Solutions 1-11

--- jQuery-1/jQuery.js
+++ jQuery-1/jQuery.js
@@ -23,17 +23,23 @@ If You would like to change sizes of Your div/button with on click :)
 var oldWidth, oldHeight;
 
 $(document).ready(function () {
-     oldWidth = $('#box').width();
-     oldHeight = $('#box').height();
+    oldWidth = $('#box').width();
+    oldHeight = $('#box').height();
 
     $('#btn1').on('click', function () {
-    $('#box').animate({width: '300px', height: '300px'})
+        $('#box').animate({
+            width: '300px',
+            height: '300px'
+        })
     })
 })
 
 $(document).ready(function () {
     $('#btn2').on('click', function () {
-    $('#box').animate({width: oldWidth, height: oldHeight})
+        $('#box').animate({
+            width: oldWidth,
+            height: oldHeight
+        })
     })
 })
 
@@ -41,36 +47,65 @@ $(document).ready(function () {
 Q4:
 Learn how to fadein your boxes
 */
-
+$(document).ready(function () {
+    $('#btnQ4').on('click', function () {
+        $("#div1").fadeIn(3000);
+        $("#div2").fadeIn(3000);
+        $("#div3").fadeIn(3000);
+    });
+});
 
 /*
 Q5:
 User must first accept then he or she can signup
 */
-
+$(document).ready(function () {
+    $("#accept").click(function () {
+        $("#submitbtn").attr("disabled", !this.checked);
+    });
+});
 
 /*
 Q6:
 Let them print
 */
-
+$(document).ready(function () {
+    $('#print').on('click', function () {
+        window.print();
+    })
+});
 
 /*
 Q7:
 Can username be too long? Yest it can - lets limit that
 */
+$(document).ready(function () {
+    var totalLength = 20;
+    $('#rchars').text(20);
 
+    $("#textarea").on('keyup', function () {
+        $("#textarea").attr('maxLength', 20);
+        $('#rchars').text(totalLength - $("#textarea").val().length);
+    });
+});
 
 /*
 Q8:
 Wanna make some words bolder, do it with jQuery
 */
-
+$(document).ready(function () {
+    $('.bolder').css('font-weight', 'bolder')
+});
 
 /*
 Q9:
 Add new div to your website
 */
+var btn = $('input[value="Click to add new div"');
+
+function new_div() {
+    btn.after('<div style="height: 150px; width: 150px; background-color: blue"></div>')
+}
 
 
 /*
@@ -83,7 +118,15 @@ Select values from a JSON object using jQuery
 Q11:
 Add element within ul list
 */
+$(document).ready(function () {
 
+    var btn = $('#btnclick');
+    var ul = $('#header ul');
+
+    btn.on('click', function () {
+        ul.append('<li><a href="#">New Element</a></li>');
+    });
+});
 
 /*
 Q12:
